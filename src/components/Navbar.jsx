ce7db56030acb176418f5d8cde4d45dae72582b5
anisha stha <stha.anisha16@gmail.com>
2024-07-25 20:30:57 +0545

    ✨feat:Navbar and Hero page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../assets/profileLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6 ">
+      <div className="flex flex-shrink-0 h-4 items-center">
+        <img src={logo} alt="logo" className="mx-2 w-10 h-8" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

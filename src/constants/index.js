c6cb7d28cdbfd93bd389a1b4848196c5fcb77a81
anisha stha <stha.anisha16@gmail.com>
2024-08-19 15:22:43 +0545

    ✨feat:contact section and📱responsive design

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+ import project1 from "../assets/projects/project-1.jpg";
+ import project2 from "../assets/projects/project-2.jpg";
+ import project3 from "../assets/projects/project-3.jpg";
+ import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -41,7 +41,7 @@ export const EXPERIENCES = [
 export const PROJECTS = [
     {
         title: "E-Commerce Website",
-        image: project1,
+         image: project1,
         description:
             "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
         technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
@@ -62,7 +62,7 @@ export const PROJECTS = [
     },
     {
         title: "Blogging Platform",
-        image: project4,
+         image: project4,
         description:
             "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
         technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],

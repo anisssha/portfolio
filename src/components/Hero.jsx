11e2f14d229b9c68996c751676db557f5608edda
anisha stha <stha.anisha16@gmail.com>
2024-08-19 15:17:40 +0545

    ✨feat:Complete Hero and About sections in Portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/anishaProfile.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
@@ -8,21 +18,40 @@ const Hero = () => {
       <div className="flex flex-wrap lg:flex-nowrap">
         {/* Text Section */}
         <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-start">
-          <h1 className="pb-14 text-6xl font-thin tracking-tight lg:mt-13 lg:text-5xl">
+          <motion.h1
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className="pb-14 text-6xl font-thin tracking-tight lg:mt-13 lg:text-5xl"
+          >
             Anisha Kumari Shrestha
-          </h1>
-          <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+          </motion.h1>
+          <motion.span
+            variants={container(0.5)}
+            initial="hidden"
+            animate="visible"
+            dragConstraints={{ left: -100, right: 100 }}
+            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+          >
             Full Stack Developer
-          </span>
-          <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+          </motion.span>
+        <motion.p
+            variants={container(1)}
+            initial="hidden"
+            animate="visible"
+            className="my-2 max-w-xl py-6 font-light tracking-tighter"
+          >
             {HERO_CONTENT}
-          </p>
+          </motion.p>
         </div>
 
         {/* Image Section */}
         <div className="w-full lg:w-1/2 flex justify-center lg:justify-end items-center lg:p-8">
           <div className="max-w-sm lg:max-w-md mt-[-20px]">
-            {/* <img
+            {/* <motion.img
+            initial={{x:100, opacity:0}}
+            animate={{x:0,opacity:1}}
+            transition={{duration:1 ,delay:1.2}}
               src={profilePic}
               alt="Anisha Shrestha"
               className="w-full h-auto object-cover"

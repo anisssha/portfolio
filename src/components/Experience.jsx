e22c509d0b3bea446b589db07fa3828dad8ddd8a
anisha stha <stha.anisha16@gmail.com>
2026-09-20 12:38:55 +0545

    ✨ refactor About and Experience components to remove framer-motion animations for improved performance

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,34 +1,18 @@
 import { EXPERIENCES } from "../constants";
-import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
     <div className="section-border border-b pb-4">
-      <motion.h1
-        whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl"
-      >
+      <h1 className="my-20 text-center text-4xl">
         Experience
-      </motion.h1>
+      </h1>
       <div>
         {EXPERIENCES.map((experience, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
+            <div className="w-full lg:w-1/4">
               <p className="muted-text mb-2 text-sm">{experience.year}</p>
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4 "
-            >
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4 ">
               <h6 className="mb-2 font-semibold">
                 {experience.role}-{" "}
                 <span className="accent-text text-sm">
@@ -44,7 +28,7 @@ const Experience = () => {
                   {tech}
                 </span>
               ))}
-            </motion.div>
+            </div>
           </div>
         ))}
       </div>
